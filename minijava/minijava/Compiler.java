e8e38b2fb4f9070d84db6dabef365021ecd1e373
eltermann <felipe.eltermann@gmail.com>
2011-03-14 09:05:21 -0300

    Portando arquivos do postfix para minijava. Próximos passos: 1- setar uma instalação local que funcione; 2- entender o Translate

---
+++ minijava/minijava/Compiler.java
@@ -0,0 +1,33 @@
+package minijava;
+import minijava.parser.*;
+import minijava.lexer.*;
+import minijava.node.*;
+import java.io.*;
+
+public class Compiler { 
+
+  public static void main(String[] arguments) { 
+    try { 
+      System.out.println("Starting compiler...");
+      
+      // Create a Parser instance. 
+      Parser p = 
+        new Parser( 
+            new Lexer( 
+              new PushbackReader( 
+                new InputStreamReader(System.in), 1024)));
+
+      // Parse the input. 
+      Start tree = p.parse();
+      System.out.println("\nResult:");
+      // Apply the translation. 
+      tree.apply(new Translation()); 
+      System.out.println("\n");
+      
+    } 
+    catch(Exception e) { 
+      System.out.println(e.getMessage()); 
+    } 
+  } 
+
+} 
