9891a242fdbe4e8a0f84525087f54490fe92b2a3
David Kurka <david.kurka@gmail.com>
2011-03-30 14:50:18 -0300

    make uglyprint more readable

--- minijava/minijava/UglyPrint.java
+++ minijava/minijava/UglyPrint.java
@@ -7,9 +7,44 @@ import minijava.analysis.*;
 
 public class UglyPrint extends DepthFirstAdapter
 {
-    public UglyPrint()
+    private int ident;
+    private boolean printSpace;
+	
+	public UglyPrint()
     {
         super();
+		ident = 0;
+		printSpace = true;
+    }
+    
+    private void identRight()
+    {
+	ident += 4;
+    }
+	
+    private void identLeft()
+    {
+	ident -= 4;
+    }
+	
+    private void print(String s)
+    {
+	if ( printSpace )
+	    for (int i = 0; i < ident; i++ )
+		System.out.print(" ");
+	System.out.print(s);
+		
+	printSpace = false;
+    }
+	
+    private void println(String s)
+    {
+	if ( printSpace )
+	    for (int i = 0; i < ident; i++ )
+		System.out.print(" ");
+	System.out.println(s);
+		
+	printSpace = true;
     }
     
 	public void defaultIn(Node node)
@@ -17,6 +52,12 @@ public class UglyPrint extends DepthFirstAdapter
 		String className[];
 		className = node.getClass().toString().split("\\.");
 		//className = className[className.length - 1];
-        System.out.print(">>" + className[className.length-1] + "\n");
+        println(">>" + className[className.length-1]);
+		identRight();
+    }
+	
+	public void defaultOut(Node node)
+    {
+		identLeft();
     }
 }
