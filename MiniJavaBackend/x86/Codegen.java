7b5c931e2056625a9157577dd6598dae08128ebf
eltermann <felipe.eltermann@gmail.com>
2011-06-15 22:25:53 -0300

    Translating Tabs to 2 Spaces

--- MiniJavaBackend/x86/Codegen.java
+++ MiniJavaBackend/x86/Codegen.java
@@ -5,11 +5,26 @@ import tree.*;
 import temp.*;
 import util.List;
 
+/**
+ * Performs instruction selection for x86.
+ *
+ * Intermediate Representation (IR) has the following possible node types:
+ *  - expstm
+ *  - stm
+ *  - exp
+ *  - label
+ *  - move
+ *  - temp
+ *  - call
+ *  - name
+ *  - const
+ *  - jump
+ */
 public class Codegen {
 
   Frame frame;
   public Codegen(Frame f) {
-    frame=f;        
+    frame=f;
   }
 
   private List<Instr> ilist = null;
@@ -44,68 +59,68 @@ public class Codegen {
 			throw new Error("Unhandled: " + s.getClass());
     }
   }
-	
-	private void munchMove(MOVE s) {
-		if (s.dst instanceof MEM) {
-			munchMove((MEM) s.dst, s.src);
+
+  private void munchMove(MOVE s) {
+    if (s.dst instanceof MEM) {
+      munchMove((MEM) s.dst, s.src);
     }
-		else {
-			munchMove((TEMP) s.dst, s.src);
+    else {
+      munchMove((TEMP) s.dst, s.src);
     }
-	}
+  }
 
-	private void munchMove(MEM d, Exp s) {
-		Temp val = munchExp(s);
-		Temp add = munchExp(d.exp);
+  private void munchMove(MEM d, Exp s) {
+    Temp val = munchExp(s);
+    Temp add = munchExp(d.exp);
 
-		emit(new assem.OPER("mov ['s0], 's1",
-			null,
-			new List<Temp>(add, new List<Temp>(val, null))));
-		return;
-	}
+    emit(new assem.OPER("mov ['s0], 's1",
+                        null,
+                        new List<Temp>(add, new List<Temp>(val, null))));
+    return;
+  }
 
-	private void munchMove(TEMP d, Exp s) {
-		Temp val = munchExp(s);
-		emit(new assem.OPER("mov 'd0, 's0",
-							new List<Temp>(d, null),
-							new List<Temp>(val, null)));
-		return;
-	}	
+  private void munchMove(TEMP d, Exp s) {
+    Temp val = munchExp(s);
+    emit(new assem.OPER("mov 'd0, 's0",
+                        new List<Temp>(d, null),
+                        new List<Temp>(val, null)));
+    return;
+  }
 
-	private void munchExpStm(EXPSTM s) {
-		Temp exp = munchExp(s.exp);
-		//TODO: emit(...)
-		return;
-	}
+  private void munchExpStm(EXPSTM s) {
+    Temp exp = munchExp(s.exp);
+    //TODO: emit(...)
+    return;
+  }
 
-	private void munchCjump(CJUMP s) {
-		//TODO
-	}
-	private void munchLabel(LABEL s) {
-		//TODO
-	}
-	private void munchJump(JUMP s) {
-		if (s.exp instanceof NAME) {
-			NAME l = (NAME) s.exp;
-			emit(new assem.OPER("jmp 'j0", 
-							null,
-							null,
-							new List<Label>(l.label, null)));
-		}
-		else {
-			Temp target = munchExp(s.exp);
-			emit(new assem.OPER("jmp 's0",
-							null,
-							new List<Temp>(target,null),
-							s.targets));
-		}
-		return;
-	}
-  
-	private Temp munchExp(Exp e) {
-		//TODO
-	}
-  
+  private void munchCjump(CJUMP s) {
+    //TODO
+  }
+  private void munchLabel(LABEL s) {
+    //TODO
+  }
+  private void munchJump(JUMP s) {
+    if (s.exp instanceof NAME) {
+      NAME l = (NAME) s.exp;
+      emit(new assem.OPER("jmp 'j0",
+                          null,
+                          null,
+                          new List<Label>(l.label, null)));
+    }
+    else {
+      Temp target = munchExp(s.exp);
+      emit(new assem.OPER("jmp 's0",
+                          null,
+                          new List<Temp>(target,null),
+                          s.targets));
+    }
+    return;
+  }
+
+  private Temp munchExp(Exp e) {
+    //TODO
+  }
+
 
   /*-------------------------------------------------------------*
    *                              MAIN                           *
@@ -117,10 +132,10 @@ public class Codegen {
     ilist = last = null;
     return l;
   }
-    
+
   List<Instr> codegen(List<Stm> body) {
     List<Instr> l = null, t = null;
-        
+
     for( ; body != null; body = body.tail ) {
       munchStm(body.head);
       if (l == null) {
