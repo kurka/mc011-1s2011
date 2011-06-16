00fe98152d17793f0ecc1c8ef3ced8e79441c0b4
eltermann <felipe.eltermann@gmail.com>
2011-06-15 21:33:37 -0300

    PrettyPrint and code tests

---
+++ MiniJavaBackend/testes/ok/1.java
@@ -0,0 +1,14 @@
+class A {
+  public static void main(String[] args) {
+    System.out.println(1234);
+  }
+
+}
+
+class B {
+  public int func(int arg) {
+    int local;
+    local = arg + 42;
+    return local;
+  }
+}
