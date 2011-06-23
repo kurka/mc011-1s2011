e2b4ccb832db7bc3e6efde4728ecc263ffb1b8c9
eltermann <felipe.eltermann@gmail.com>
2011-06-22 21:23:38 -0300

    Adding example and GCC calls in Makefile

---
+++ MiniJavaBackend/testes/ok/3.java
@@ -0,0 +1,18 @@
+class Factorial{
+  public static void main(String[] a){
+    System.out.println(123);
+  }
+}
+
+class Fac {
+
+  public int ComputeFac(int num){
+    int num_aux ;
+    if (num < 6)
+	    num_aux = num;
+    else 
+	    num_aux = 7;
+    return 3 * num_aux;
+  }
+  
+}
